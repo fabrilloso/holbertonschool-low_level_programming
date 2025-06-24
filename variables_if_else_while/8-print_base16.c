e1865cdc17a28716bd095f96f27f1233f6574c68
fabrilloso <fabriciohernanbs@gmail.com>
2025-06-24 20:06:56 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints number from 0 to 9
+* adding letters from a to f
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+	char d;
+
+	for (c = 0; c <= 9; c++)
+		putchar('0' + c);
+	for (d = 'a'; d <= 'f'; d++)
+	{	
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
