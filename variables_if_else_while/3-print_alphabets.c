f92a428104575e342c1b0dd838f2a3c266c713e4
fabrilloso <fabriciohernanbs@gmail.com>
2025-06-24 15:46:11 +0000

    Upload the C file

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints alphabetes in lower case followed by a new line
+*
+* Return: Lowercase followed by a new line.
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
