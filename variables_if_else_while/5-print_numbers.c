377367897390c65380c9564b9d0411536974d252
fabrilloso <fabriciohernanbs@gmail.com>
2025-06-24 18:56:35 +0000

    5-print

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints number from 0 to 9
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+
+	return (0);
+}
