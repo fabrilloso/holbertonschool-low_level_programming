ccf6d49e91202b6f775be7a1470fab276850ace4
fabrilloso <fabriciohernanbs@gmail.com>
2025-06-27 18:45:26 +0000

    troska

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+
+	else {
+		return (0);
+	}
+}
